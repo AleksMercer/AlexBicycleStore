cbc56df810aaa6ad9d2774e799fe851ede472b18
AleksMercer <Kalagaev.Alex.A@gmail.com>
2025-10-08 22:23:37 +0300

    feat: init project

---
+++ pages/_app.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+import "../styles/globals.css";
+import "../styles/grid.css";
+import "boxicons/css/boxicons.min.css";
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>Alex K Bikes</title>
+        <meta
+          name="description"
+          content="Bikes E-commerce store landing page"
+        />
+        <link rel="icon" href="/bike.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default MyApp;
