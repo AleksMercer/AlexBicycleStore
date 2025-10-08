d357f7284f4e7773a2dc791184e0f7611b8404e4
AleksMercer <Kalagaev.Alex.A@gmail.com>
2025-10-08 22:39:48 +0300

    fix: prepare for gh-pages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-}
+  output: 'export',
+  basePath: '/AlexBicycleStore',
+  assetPrefix: '/AlexBicycleStore/',
+  images: { unoptimized: true },
+};
+
+module.exports = nextConfig;
