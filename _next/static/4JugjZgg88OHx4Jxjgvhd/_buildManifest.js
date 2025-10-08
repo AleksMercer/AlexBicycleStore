f163d1780632b88279e42852968da51de1455b1b
AleksMercer <Kalagaev.Alex.A@gmail.com>
2025-10-09 00:32:45 +0300

    Updates

---
+++ _next/static/4JugjZgg88OHx4Jxjgvhd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-cbeee0f95d9e6c0d.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
